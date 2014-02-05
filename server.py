4d09f43a52d76991feec441f033a23c62f8a6b87
Ben Taylor <taylo886@msu.edu>
2014-02-05 13:57:24 -0500

    For some reason POST-ed 404s don't work quite right yet

--- server.py
+++ server.py
@@ -4,57 +4,11 @@ import socket
 import time
 from urlparse import urlparse, parse_qs
 import cgi
-import StringIO
-import re
+from StringIO import StringIO
 import jinja2
 
-def handle_get(conn, path):
-    args = parse_qs(urlparse(path)[4])
-    response = {'/' : 'index.html', \
-              '/content' : 'content.html', \
-              '/file' : 'file.html', \
-              '/image' : 'image.html', \
-              '/form' : 'form.html', \
-              '/submit' : 'submit.html', \
-             }
-    loader = jinja2.FileSystemLoader('./templates')
-    env = jinja2.Environment(loader=loader)
-    retval = 'HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'
-    try:
-        path = response[urlparse(path)[2]]
-    except KeyError:
-        args['path'] = path
-        retval = 'HTTP/1.0 404 Not Found\r\n'
-        path = '404.html'
-    template = env.get_template(path)
-    retval += template.render(args)
-    conn.send(retval)
-
-def handle_post(conn, path, **kwargs):
-    arg0 = StringIO.StringIO(kwargs['data'])
-    kwargs.pop('data')
-
-    e = {}
-    e['REQUEST_METHOD'] = 'POST'
-
-    data = cgi.FieldStorage(fp=arg0, headers=kwargs, environ=e)
-
-    args = dict([(x, [data[x].value]) for x in data.keys()])
-    response = {'/submit' : 'submit.html',}
-    loader = jinja2.FileSystemLoader('./templates')
-    env = jinja2.Environment(loader=loader)
-    retval = 'HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'
-    try:
-        path = response[urlparse(path)[2]]
-    except KeyError:
-        args['path'] = path
-        retval = 'HTTP/1.0 404 Not Found\r\n'
-        path = '404.html'
-    template = env.get_template(path)
-    retval += template.render(args)
-    conn.send(retval)
-
 def handle_connection(conn):
+    # Start reading in data from the connection
     req = conn.recv(1)
     count = 0
     while req[-4:] != '\r\n\r\n':
@@ -64,19 +18,54 @@ def handle_connection(conn):
     for line in headers.split('\r\n')[:-2]:
         k, v = line.split(': ', 1)
         d[k.lower()] = v
+
+    # Parse out the path and related info
+    path = urlparse(req.split(' ', 3)[1])
+
+    # The dict of pages we know how to get to
+    response = {'/' : 'index.html', \
+              '/content' : 'content.html', \
+              '/file' : 'file.html', \
+              '/image' : 'image.html', \
+              '/form' : 'form.html', \
+              '/submit' : 'submit.html', \
+             }
+
+    # Basic connection information and set up templates
+    loader = jinja2.FileSystemLoader('./templates')
+    env = jinja2.Environment(loader=loader)
+    retval = 'HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'
+    args = {}
+
+    # Check if the request is get or post, set up the args
     if req.startswith('GET '):
-        path = req.split(' ', 3)[1]
-        handle_get(conn, path)
+        args = parse_qs(path[4])
     elif req.startswith('POST '):
-        path = req.split(' ', 3)[1]
         l = int(d['content-length'])
         data = ''
         while len(data) < l:
             data += conn.recv(1)
-        d['data'] = data
-        handle_post(conn, path, **d)
-    else:
-        print req
+        e = {'REQUEST_METHOD' : 'POST'}
+        d = cgi.FieldStorage(fp=StringIO(data), headers=args, environ=e)
+        try:
+            args = dict([(x, [d[x].value]) for x in d.keys()])
+        except AttributeError:
+            pass
+
+    # Check if we got a path to an existing page
+    try:
+        # Load the appropriate template
+        template = env.get_template(response[path[2]])
+    except KeyError:
+        args['path'] = path[2]
+        retval = 'HTTP/1.0 404 Not Found\r\n'
+        template = env.get_template('404.html')
+
+    # Render the page
+    print template
+    retval += template.render(args)
+    conn.send(retval)
+
     # Done here
     conn.close()
 
@@ -98,7 +87,6 @@ def main():
         print 'Got connection from', client_host, client_port
         handle_connection(c)
         
-
 # boilerplate
 if __name__ == "__main__":
     main()
