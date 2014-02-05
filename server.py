3cef2eb35b83b2fbca947f0d5fc175def9b204c6
Ben Taylor <taylo886@msu.edu>
2014-02-05 17:08:47 -0500

    ooooh... ahhhh... pretty

--- server.py
+++ server.py
@@ -15,45 +15,41 @@ def handle_connection(conn):
         req += conn.recv(1)
 
     # Parse the headers we've received
-    req, headers = req.split('\r\n',1)
-    d = {}
-    for line in headers.split('\r\n')[:-2]:
+    req, data = req.split('\r\n',1)
+    headers = {}
+    for line in data.split('\r\n')[:-2]:
         k, v = line.split(': ', 1)
-        d[k.lower()] = v
+        headers[k.lower()] = v
 
     # Parse out the path and related info
     path = urlparse(req.split(' ', 3)[1])
 
     # The dict of pages we know how to get to
-    response = {'/' : 'index.html', \
-              '/content' : 'content.html', \
-              '/file' : 'file.html', \
-              '/image' : 'image.html', \
-              '/form' : 'form.html', \
-              '/submit' : 'submit.html', \
-             }
+    response = {
+                '/'        : 'index.html',   \
+                '/content' : 'content.html', \
+                '/file'    : 'file.html',    \
+                '/image'   : 'image.html',   \
+                '/form'    : 'form.html',    \
+                '/submit'  : 'submit.html',  \
+               }
 
     # Basic connection information and set up templates
     loader = jinja2.FileSystemLoader('./templates')
     env = jinja2.Environment(loader=loader)
     retval = 'HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'
-    args = {}
+    content = ''
 
     # Check if the request is get or post, set up the args
-    if req.startswith('GET '):
-        args = parse_qs(path[4])
-    elif req.startswith('POST '):
-        l = int(d['content-length'])
-        data = ''
-        while len(data) < l:
-            data += conn.recv(1)
-        e = {'REQUEST_METHOD' : 'POST'}
-        fs = cgi.FieldStorage(fp=StringIO(data), headers=d, environ=e)
-        args = dict([(x, [fs[x].value]) for x in fs.keys()])
+    args = parse_qs(path[4])
+    if req.startswith('POST '):
+        while len(content) < int(headers['content-length']):
+            content += conn.recv(1)
+    fs = cgi.FieldStorage(fp=StringIO(content), headers=headers, environ={'REQUEST_METHOD' : 'POST'})
+    args.update(dict([(x, [fs[x].value]) for x in fs.keys()]))
 
     # Check if we got a path to an existing page
     try:
-        # Load the appropriate template
         template = env.get_template(response[path[2]])
     except KeyError:
         args['path'] = path[2]
